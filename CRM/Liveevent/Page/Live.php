e8b1db38762be418c5d643a894ab93afdfdec0e8
xavier <xavier@tttp.eu>
2016-10-06 08:02:19 +0200

    ready for demo

--- CRM/Liveevent/Page/Live.php
+++ CRM/Liveevent/Page/Live.php
@@ -1,30 +1,22 @@
 <?php
 
 require_once 'CRM/Core/Page.php';
+require_once 'api/class.api.php';
 
 class CRM_Liveevent_Page_Live extends CRM_Core_Page {
   function run() {
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
 
   $api= new civicrm_api3();
-  $event_id=$event_id + 0;
+  $event_id= CRM_Utils_Request::retrieve('event_id', 'Positive');
   if ($event_id ==0) {
-    die (" civicrm/event/live/{event_id}");
+    die (" civicrm/event/live?event_id={event_id}");
   }
-  if (!$api->OptionGroup->GetSingle (array('name'=>'participant_role'))){
+  if (!$api->OptionValue->Get (array('option_group_id'=>'participant_role'))){
     die ("FATAL: ".$api->attr('error_message'));
   }
-  $role_group = $api->attr('id');
-  if (!$api->OptionValue->Get (array('option_group_id'=>$role_group))){
-    die ("FATAL: ".$api->attr('error_message'));
-  }
-  if (defined( 'TTTP_LISTING_FIX_JQUERY_CONFLICT'))
-    $this->assign('currentTime', date('Y-m-d H:i:s'));
-    $this->assign('cj',"jQuery");
-  else  
-    $this->assign('cj',"cj");
   $this->assign('role',$api->values());
-  if (!$api->Event->GetSingle ($event_id)){
+  if (!$api->Event->GetSingle (array("event_id"=>$event_id))){
     die ("FATAL: ".$api->attr('error_message'));
   }
   CRM_Utils_System::setTitle($api->attr('title'));;
@@ -36,7 +28,6 @@ class CRM_Liveevent_Page_Live extends CRM_Core_Page {
   $people = $api->values();
   $this->assign('total',count($people));
   $this->assign('participants_json',json_encode($people));
-  $this->assign('tttp_root', drupal_get_path('module', 'tttp_listing') );
 
 
     parent::run();
